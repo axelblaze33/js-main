f8df3565d4642edc07f1346cb452e084df8a036a
axelblaze33 <malshikharepranav123@gmail.com>
2024-07-30 11:08:11 +0530

    first commit

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state; //undefined
+// let state = null //null not undefined
+
+//PRIMITIVE DATA TYPES
+// number => range 2 to power 53
+// bigint => used for high level programs like reditt twitter
+// string => "" or '' but double quotes is preferred
+// boolean => true/false  only true or false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+
+// object
+console.log(typeof "Pranav");
+console.log(typeof undefined); // output => undefined
+console.log(typeof null); // output =>object
